f89bf7ff9ec6858bbd9cc2419c5a6d89719b4686
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-10-11 14:00:13 +0100

    Add new solution to task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,10 +3,6 @@
 
 
 def concat(str1: str, str2: str) -> str:
-    """ Returns the concatenation of two strings."""
-    return "{} {}".format(str1, str2)
-
-
-# if __name__ == "__main__":
-#     print(concat("Hello", "World"))
-#     print(concat.__annotations__)
+    '''Concatenates two strings.
+    '''
+    return str1 + str2
